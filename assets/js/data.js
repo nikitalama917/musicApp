829789c84018108232a3d9897ff4a4631c63b44b
nikitalama <Mydaddy1>
2022-08-14 19:42:54 -0400

    music Player

---
+++ assets/js/data.js
@@ -0,0 +1,62 @@
+let songs = [
+    {
+        name: 'What Once Was',
+        path: 'assets/music/whatoncewas.mp3',
+        artist: "her's",
+        cover: 'assets/img/her.jpg'
+    },
+    {
+        name: 'Out of Time',
+        path: 'assets/music/outoftime.mp3',
+        artist: 'The Weekend',
+        cover: 'assets/img/weekend.jpeg'
+    },
+    {
+        name: "In Love",
+        path: 'assets/music/inlove.mp3',
+        artist: "Bane's World",
+        cover: 'assets/img/bane.jpg'
+    },
+    {
+        name: "Paris in the Rain",
+        path: 'assets/music/rain.mp3',
+        artist: "Lauv",
+        cover: 'assets/img/paris.jpg'
+    },
+    {
+        name: "Until I found You",
+        path: 'assets/music/found.mp3',
+        artist: "Stephen Sanchez",
+        cover: 'assets/img/steve.jpg'
+    },
+    {
+        name: "Make You Mine",
+        path: 'assets/music/makeyou.mp3',
+        artist: "PUBLIC",
+        cover: 'assets/img/mine.jpg'
+    },
+    {
+        name: "Die For You",
+        path: 'assets/music/diefor.mp3',
+        artist: "The Weekend",
+        cover: 'assets/img/star.jpg'
+    },
+    {
+        name: "Late Night Talking",
+        path: 'assets/music/talking.mp3',
+        artist: "Harry Styles",
+        cover: 'assets/img/harry.jpg'
+    },
+    {
+        name: 'She Needs Him',
+        path: 'assets/music/need.mp3',
+        artist: "her's",
+        cover: 'assets/img/she.jpg'
+    },
+    {
+        name: "Mantramughdha",
+        path: 'assets/music/mantra.mp3',
+        artist: "Satish",
+        cover: 'assets/img/nepali.jpg'
+    }
+];
